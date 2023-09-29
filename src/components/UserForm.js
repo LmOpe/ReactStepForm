5b07e36ae2929e1de9598365f76367e24a6d1013
LmOpe <lawalmuhammed44@gmail.com>
2023-09-29 09:06:01 +0100

    Initial commit

---
+++ src/components/UserForm.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+    state = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        occupation: '',
+        city: '',
+        bio: ''
+    }
+
+    // Proceed to next step
+    nextStep = () => {
+        
+    }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default UserForm
